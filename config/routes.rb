27786f8c823777754b4cea88b0cf5dc347666924
ryo-terada <ryo214099@gmail.com>
2022-05-27 06:36:21 +0000

    [Finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 root to: 'homes#top'
 devise_for :users
   
-resources :post_images, only: [:new, :create, :index, :show]
+resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit, :update]
 
 get 'homes/about', as: 'about'
 
